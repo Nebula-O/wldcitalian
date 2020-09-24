d162ff87564bebf1115efdee8ef057a27e3c036c
Nebula-O <nebula.1.0.one@gmail.com>
2020-09-24 20:06:17 +0100

    I didn't read properly lol

--- commands/tren.js
+++ commands/tren.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(msg, args) {
         const {google} = require('googleapis');
         const apiKey = "579bd4f236802db9469b9ab341c02b789ab1596f";
-        var googleTranslate = require('google-translate')(apiKey, options);
+        var googleTranslate = require('google-translate')(apiKey, { concurrentLimit: 20 });
         googleTranslate.translate(args, "it", function(err, translation) {
           console.log("Translated " + args + " to " + translation.translatedText);
           msg.reply(translation.translatedText);
