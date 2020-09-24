f9c538c697c777fc44559a852c6e55356f03a1f8
Nebula-O <nebula.1.0.one@gmail.com>
2020-09-24 20:03:16 +0100

    Let's hope this works.

--- commands/trit.js
+++ commands/trit.js
@@ -2,6 +2,8 @@ module.exports = {
     name: '.trit',
     description: 'Trit!',
     execute(msg, args) {
+        const {google} = require('googleapis');
+        const apiKey = "579bd4f236802db9469b9ab341c02b789ab1596f";
         var googleTranslate = require('google-translate')(apiKey, options);
         googleTranslate.translate(args, "en", function(err, translation) {
           console.log("Translated " + args + " to " + translation.translatedText);
