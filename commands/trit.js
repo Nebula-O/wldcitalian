11a505a7e1e961c38918623876f9ae8af27a8696
Nebula-O <nebula.1.0.one@gmail.com>
2020-09-24 20:17:10 +0100

    Work please

--- commands/trit.js
+++ commands/trit.js
@@ -4,8 +4,8 @@ module.exports = {
     execute(msg, args) {
         const {google} = require('googleapis');
         const apiKey = "579bd4f236802db9469b9ab341c02b789ab1596f";
-        var googleTranslate = require('google-translate')(apiKey,  { concurrentLimit: 20 });
-        google.translate(args, "en", function(err, translation) {
+        var googleTranslate = require('google-translate');
+        googleTranslate.translate(args, "en", function(err, translation) {
           console.log("Translated " + args + " to " + translation.translatedText);
           msg.reply(translation.translatedText);
         });
