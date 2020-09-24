91ae8c5549c188e93a092f2bb7a72d3830133437
Nebula-O <nebula.1.0.one@gmail.com>
2020-09-24 15:57:00 +0100

    .gitignore added

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'help',
+    name: '.help',
     description: 'Help me!',
     execute(msg, args) {
       msg.channel.send('__Help__```.ping\n.help``````-Work in progress-\n.dailywords\n.score\n.scores\n.toeng <italian word>\n.toitl <english word>\n.add <english word> <italian equivalent>\n.listwords\n.dictlookup <first letter>\n.dailywords```');
