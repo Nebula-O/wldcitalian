c658a26f7e63e6969c857964cd2346d224c870e3
Nebula-O <nebula.1.0.one@gmail.com>
2020-09-24 20:32:06 +0100

    Using new API

--- commands/tren.js
+++ commands/tren.js
@@ -2,12 +2,12 @@ module.exports = {
     name: '.tren',
     description: 'Tren!',
     execute(msg, args) {
-        const {google} = require('googleapis');
-        const apiKey = "579bd4f236802db9469b9ab341c02b789ab1596f";
-        var googleTranslate = require('google-translate');
-        google.translate(args, "it", function(err, translation) {
+        const translate = require('@vitalets/google-translate-api');
+        translate(args, { to:'it'}).then(res => {
           console.log("Translated " + args + " to " + translation.translatedText);
           msg.reply(translation.translatedText);
+        }).catch(err => {
+            console.error(err);
         });
     },
   };
--- commands/trit.js
+++ commands/trit.js
@@ -2,12 +2,12 @@ module.exports = {
     name: '.trit',
     description: 'Trit!',
     execute(msg, args) {
-        const {google} = require('googleapis');
-        const apiKey = "579bd4f236802db9469b9ab341c02b789ab1596f";
-        var googleTranslate = require('google-translate');
-        googleTranslate.translate(args, "en", function(err, translation) {
+        const translate = require('@vitalets/google-translate-api');
+        translate(args, { to:'en'}).then(res => {
           console.log("Translated " + args + " to " + translation.translatedText);
           msg.reply(translation.translatedText);
+        }).catch(err => {
+            console.error(err);
         });
     },
   };
