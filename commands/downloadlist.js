0f64738a26c660a2f61a6afb7e72c447877b7709
Nebula-O <nebula.1.0.one@gmail.com>
2020-10-04 18:36:10 +0100

    Updated .download

--- commands/downloadlist.js
+++ commands/downloadlist.js
@@ -5,9 +5,11 @@ module.exports = {
     description: 'Download the words file!',
     execute(msg, args) {
         const fs = require('fs');
-        const content = fs.readFileSync('words.txt', 'utf-8');
+        //const content = fs.readFileSync('words.txt', 'utf-8');
+        const fileSizeInBytes = fs.statSync('words.txt').size;
+        const fileSizeInMegabytes = fileSizeInBytes / 1000000.0;
         const attatchment = new Discord.Attachment('words.txt');
-        msg.channel.send(msg.author, attatchment);
+        msg.channel.send(msg.author+" "+fileSizeInMegabytes, attatchment);
     },
   };
   
