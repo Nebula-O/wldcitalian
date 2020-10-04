678dcef835e8ddfe1edc146ccad497d5f3679f90
Nebula-O <nebula.1.0.one@gmail.com>
2020-10-04 12:49:03 +0100

    Code change

--- commands/search.js
+++ commands/search.js
@@ -54,16 +54,12 @@ module.exports = {
     const prefix = args[0];
 
     for(var word in allWords){
-
-      if((word.split('=')[0]).startsWith(prefix)){
-        word = word.replaceAll('=', ' = ');
-        replyStr = replyStr.concat(' `' + word + '`, ');
-      }
-
-      else if(word.split('=')[1].startsWith(prefix)){
-        word = word.replaceAll('=', ' = ');
-        replyStr = replyStr.concat(' `' + word + '`, ');
-      }
+      var substrings = word.split('=').forEach((str) => {
+        if(str.startsWith(prefix)){
+          word = word.replaceAll('=', ' = ');
+          replyStr = replyStr.concat(' `' + word + '`, ');
+        }
+      });
     }
     
     if(allWords.length < 1){
